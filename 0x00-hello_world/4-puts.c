7f955206ddbe10132f477164109c1eac25fd9db2
Dam1lola <damsybaby102@gmail.com>
2022-03-10 20:05:15 -0800

    commit this

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Description: main - prints out strings to stdout
+ *  
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
