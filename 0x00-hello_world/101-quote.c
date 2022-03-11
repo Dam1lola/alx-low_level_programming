1d05a5bf70da760e3634f07af0c8dcad125fcccc
Dam1lola <damsybaby102@gmail.com>
2022-03-10 20:33:04 -0800

    commit this

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 /**
  * main - prints exactly "and that pieceof art is useful"
- *  - Dora korpar, 2015-10-19,
+ * - Dora korpar, 2015-10-19,
  *  followed by a new line, to the standard error.
  *  Return: Always 0 (Success)
  */
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
  * Description: main - prints out strings to stdout
  *  
@@ -8,6 +7,7 @@
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+  puts("\"programming is like building a multilingual puzzle");
+    return (0);
+
 }
