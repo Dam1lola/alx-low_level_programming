c44b8d1aec158c0f9979e2f50275f6f1bd8825a4
Dam1lola <damsybaby102@gmail.com>
2022-03-13 16:19:49 -0700

    commigt variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,9 @@
  *  Return: Always 0.
  *
  */
+
 int main(void)
 {
-
 		int n;
 
 		srand(time(0));
@@ -21,7 +21,6 @@ int main(void)
 					if ((n % 10) > 5)
 					{
 						printf("Last digit of %d is %d and is greater than 5\n",
-								
 								n, n % 10);
 					}
 					
@@ -30,20 +29,15 @@ int main(void)
 					{
 						printf("Last digit of %d is %d and is less than 6 and not 0\n",
 								n, n % 10);
-
 					}
 					
 					else
 					
-					
 					{
-						
 						printf("Last digit of %d is %d and is 0\n",
-								
 								n, n % 10);
 					
 					}
-					
 					
 					return (0);
 
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,7 +5,6 @@
  * Return: Always 0.
  *
  */
-
 int main(void)
 {
 	char letter;
@@ -20,5 +19,4 @@ int main(void)
 	putchar('\n');
 	
 	return (0);
-
 }
