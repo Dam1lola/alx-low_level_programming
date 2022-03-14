573cc95ce5fe86e4a5027b263c155990c9a0fa62
Dam1lola <damsybaby102@gmail.com>
2022-03-14 15:27:12 -0700

    commit variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int digit1, digit2;
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -20,7 +21,6 @@ int main(void)
 				continue;
 			putchar(',');
 			putchar(' ');
-		
 		}
 	}
 	putchar('\n');
