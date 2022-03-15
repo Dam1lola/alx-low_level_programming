2eabba89e2db9924850cfacea2aa01275adae5a3
Dam1lola <damsybaby102@gmail.com>
2022-03-14 17:43:20 -0700

    commit this

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,10 @@
  * main - Prints exactly "and that piece of art is useful"
  * - Dora korpar, 2015-10-19,
  * followed by a new line, to the standard error.
+ *
  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
