c33585f0b476055d48c2ca0468765e3a6b4fb334
Dam1lola <damsybaby102@gmail.com>
2022-03-22 10:32:37 +0000

    commit pointers

--- 0x05-pointers_arrays_string/0-reset_to_98.c
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#inculde"main.h"
+#inculde<stdio.h>
 /**
  * reset_to_98 - updates a integer value to 98
  * @n: A pointer to an int that will be updated
@@ -7,7 +7,7 @@
  */
 
 void reset_to_98(int *n)
-{
+
 *n = 98;
 }
 
