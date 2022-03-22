e52bc053b938d6b9814194a22e277cd82784e0bb
Dam1lola <damsybaby102@gmail.com>
2022-03-22 20:26:33 +0000

    commit pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,9 +5,12 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
+
 int i, d, n, len, f, digit;
+
 i = 0;
 d = 0;
 n = 0;
@@ -27,11 +30,13 @@ if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
 if (d % 2)
-digit = -digit;
+
+	digit = -digit;
 n = n * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
+
+	break;
 
 f = 0;
 
@@ -42,7 +47,7 @@ i++;
 
 if (f == 0)
 
-return (0);
+	return (0);
 
 return (n);
 }
