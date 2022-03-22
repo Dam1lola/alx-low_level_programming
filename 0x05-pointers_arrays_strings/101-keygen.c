15daac13204d0b455d660993131274abe0c5b2e6
Dam1lola <damsybaby102@gmail.com>
2022-03-22 21:38:47 +0000

    commit pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,8 @@ int main(void)
 int pass[100];
 int i, sum, n;
 
-sum = 0;
+sum = 0;	
+
 srand(time(NULL));
 
 for (i = 0; i < 100; i++)
@@ -22,12 +23,12 @@ sum += (pass[i] + '0');
 putchar(pass[i] + '0');
 if ((2772 - sum) - '0' < 78)
 {
-n = 2772 y- sum - '0';
+n = 2772 - sum - '0';
 sum += n;
 putchar(n + '0');
 break;
 }
 }
+
 return (0);
 }
-
