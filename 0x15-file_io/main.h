7a687f14b5f5d8cd1fcdddba4c9d023a7a892b8e
Dam1lola <damsybaby102@gmail.com>
2022-05-03 06:36:48 -0700

    this way

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,25 +1,31 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
+
+
 int _putchar(char c);
 
-void print_alphabet(void);
+ssize_t read_textfile(const char *filename, size_t letters);
 
-void print_alphabet_x10(void);
+int create_file(const char *filename, char *text_content);
 
-int _islower(int c);
+int append_text_to_file(const char *filename, char *text_content);
 
-int _isalpha(int c);
 
-int print_sign(int n);
 
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+#endif /* MAIN_H */
